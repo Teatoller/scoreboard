705090491efd23e03ae9e6e949feefb8b984bef8
Teatoller <steven.otieno1@gmail.com>
2019-11-02 10:43:32 +0300

    (header component): Implement scoreboard header component
    - create scoreboard header function
    - render Header component in ReactDOM
    
    [Starts #006]

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,14 +1,10 @@
-const desc = "I just learned how to create my first react node";
+function Header() {
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+  );
+}
 
-const myTitleId = 'main-title'
-
-const name = 'Steven'
-
-const header = (
-  <header>
-    <h1 id={myTitleId} >{name}'s First React Element</h1>
-    <p>{desc}</p>
-  </header>
-);
-
-ReactDOM.render(header, document.getElementById("root"));
+ReactDOM.render(<Header />, document.getElementById("root"));
